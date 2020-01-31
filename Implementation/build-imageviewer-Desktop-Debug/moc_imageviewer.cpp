e9589749b47aea533ab353ab7b16fc580a3e8cc5
JPiljug <j.piljug@gmx.de>
2020-02-01 00:11:20 +0100

    Doc

---
+++ Implementation/build-imageviewer-Desktop-Debug/moc_imageviewer.cpp
@@ -0,0 +1,270 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'imageviewer.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../../../Backup/imageviewer.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'imageviewer.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.14.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_ImageViewer_t {
+    QByteArrayData data[39];
+    char stringdata0[406];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_ImageViewer_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_ImageViewer_t qt_meta_stringdata_ImageViewer = {
+    {
+QT_MOC_LITERAL(0, 0, 11), // "ImageViewer"
+QT_MOC_LITERAL(1, 12, 4), // "open"
+QT_MOC_LITERAL(2, 17, 0), // ""
+QT_MOC_LITERAL(3, 18, 6), // "saveAs"
+QT_MOC_LITERAL(4, 25, 5), // "print"
+QT_MOC_LITERAL(5, 31, 4), // "copy"
+QT_MOC_LITERAL(6, 36, 5), // "paste"
+QT_MOC_LITERAL(7, 42, 6), // "zoomIn"
+QT_MOC_LITERAL(8, 49, 7), // "zoomOut"
+QT_MOC_LITERAL(9, 57, 10), // "normalSize"
+QT_MOC_LITERAL(10, 68, 11), // "fitToWindow"
+QT_MOC_LITERAL(11, 80, 5), // "about"
+QT_MOC_LITERAL(12, 86, 4), // "draw"
+QT_MOC_LITERAL(13, 91, 9), // "translate"
+QT_MOC_LITERAL(14, 101, 15), // "calcTranslation"
+QT_MOC_LITERAL(15, 117, 13), // "doTranslation"
+QT_MOC_LITERAL(16, 131, 6), // "pencil"
+QT_MOC_LITERAL(17, 138, 9), // "startDraw"
+QT_MOC_LITERAL(18, 148, 5), // "lines"
+QT_MOC_LITERAL(19, 154, 13), // "notFilledRect"
+QT_MOC_LITERAL(20, 168, 10), // "filledRect"
+QT_MOC_LITERAL(21, 179, 13), // "existingColor"
+QT_MOC_LITERAL(22, 193, 8), // "newColor"
+QT_MOC_LITERAL(23, 202, 15), // "actualColorVect"
+QT_MOC_LITERAL(24, 218, 17), // "standartColorVect"
+QT_MOC_LITERAL(25, 236, 14), // "smallColorVect"
+QT_MOC_LITERAL(26, 251, 15), // "customColorVect"
+QT_MOC_LITERAL(27, 267, 11), // "setNewColor"
+QT_MOC_LITERAL(28, 279, 12), // "setDrawColor"
+QT_MOC_LITERAL(29, 292, 11), // "changeColor"
+QT_MOC_LITERAL(30, 304, 18), // "changeCurrentLayer"
+QT_MOC_LITERAL(31, 323, 13), // "updateVisible"
+QT_MOC_LITERAL(32, 337, 10), // "makeShaped"
+QT_MOC_LITERAL(33, 348, 8), // "newLayer"
+QT_MOC_LITERAL(34, 357, 11), // "addNewLayer"
+QT_MOC_LITERAL(35, 369, 8), // "setWidth"
+QT_MOC_LITERAL(36, 378, 1), // "w"
+QT_MOC_LITERAL(37, 380, 12), // "setNewLayerX"
+QT_MOC_LITERAL(38, 393, 12) // "setNewLayerY"
+
+    },
+    "ImageViewer\0open\0\0saveAs\0print\0copy\0"
+    "paste\0zoomIn\0zoomOut\0normalSize\0"
+    "fitToWindow\0about\0draw\0translate\0"
+    "calcTranslation\0doTranslation\0pencil\0"
+    "startDraw\0lines\0notFilledRect\0filledRect\0"
+    "existingColor\0newColor\0actualColorVect\0"
+    "standartColorVect\0smallColorVect\0"
+    "customColorVect\0setNewColor\0setDrawColor\0"
+    "changeColor\0changeCurrentLayer\0"
+    "updateVisible\0makeShaped\0newLayer\0"
+    "addNewLayer\0setWidth\0w\0setNewLayerX\0"
+    "setNewLayerY"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_ImageViewer[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      36,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  194,    2, 0x08 /* Private */,
+       3,    0,  195,    2, 0x08 /* Private */,
+       4,    0,  196,    2, 0x08 /* Private */,
+       5,    0,  197,    2, 0x08 /* Private */,
+       6,    0,  198,    2, 0x08 /* Private */,
+       7,    0,  199,    2, 0x08 /* Private */,
+       8,    0,  200,    2, 0x08 /* Private */,
+       9,    0,  201,    2, 0x08 /* Private */,
+      10,    0,  202,    2, 0x08 /* Private */,
+      11,    0,  203,    2, 0x08 /* Private */,
+      12,    0,  204,    2, 0x08 /* Private */,
+      13,    0,  205,    2, 0x08 /* Private */,
+      14,    0,  206,    2, 0x08 /* Private */,
+      15,    0,  207,    2, 0x08 /* Private */,
+      16,    0,  208,    2, 0x08 /* Private */,
+      17,    0,  209,    2, 0x08 /* Private */,
+      18,    0,  210,    2, 0x08 /* Private */,
+      19,    0,  211,    2, 0x08 /* Private */,
+      20,    0,  212,    2, 0x08 /* Private */,
+      21,    0,  213,    2, 0x08 /* Private */,
+      22,    0,  214,    2, 0x08 /* Private */,
+      23,    0,  215,    2, 0x08 /* Private */,
+      24,    0,  216,    2, 0x08 /* Private */,
+      25,    0,  217,    2, 0x08 /* Private */,
+      26,    0,  218,    2, 0x08 /* Private */,
+      27,    0,  219,    2, 0x08 /* Private */,
+      28,    0,  220,    2, 0x08 /* Private */,
+      29,    0,  221,    2, 0x08 /* Private */,
+      30,    0,  222,    2, 0x08 /* Private */,
+      31,    0,  223,    2, 0x08 /* Private */,
+      32,    0,  224,    2, 0x08 /* Private */,
+      33,    0,  225,    2, 0x08 /* Private */,
+      34,    0,  226,    2, 0x08 /* Private */,
+      35,    1,  227,    2, 0x08 /* Private */,
+      37,    1,  230,    2, 0x08 /* Private */,
+      38,    1,  233,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   36,
+    QMetaType::Void, QMetaType::Int,   36,
+    QMetaType::Void, QMetaType::Int,   36,
+
+       0        // eod
+};
+
+void ImageViewer::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<ImageViewer *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->open(); break;
+        case 1: _t->saveAs(); break;
+        case 2: _t->print(); break;
+        case 3: _t->copy(); break;
+        case 4: _t->paste(); break;
+        case 5: _t->zoomIn(); break;
+        case 6: _t->zoomOut(); break;
+        case 7: _t->normalSize(); break;
+        case 8: _t->fitToWindow(); break;
+        case 9: _t->about(); break;
+        case 10: _t->draw(); break;
+        case 11: _t->translate(); break;
+        case 12: _t->calcTranslation(); break;
+        case 13: _t->doTranslation(); break;
+        case 14: _t->pencil(); break;
+        case 15: _t->startDraw(); break;
+        case 16: _t->lines(); break;
+        case 17: _t->notFilledRect(); break;
+        case 18: _t->filledRect(); break;
+        case 19: _t->existingColor(); break;
+        case 20: _t->newColor(); break;
+        case 21: _t->actualColorVect(); break;
+        case 22: _t->standartColorVect(); break;
+        case 23: _t->smallColorVect(); break;
+        case 24: _t->customColorVect(); break;
+        case 25: _t->setNewColor(); break;
+        case 26: _t->setDrawColor(); break;
+        case 27: _t->changeColor(); break;
+        case 28: _t->changeCurrentLayer(); break;
+        case 29: _t->updateVisible(); break;
+        case 30: _t->makeShaped(); break;
+        case 31: _t->newLayer(); break;
+        case 32: _t->addNewLayer(); break;
+        case 33: _t->setWidth((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 34: _t->setNewLayerX((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 35: _t->setNewLayerY((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject ImageViewer::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_ImageViewer.data,
+    qt_meta_data_ImageViewer,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *ImageViewer::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ImageViewer::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ImageViewer.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int ImageViewer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 36)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 36;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 36)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 36;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
